61bd09849b4ac747ddf0683536eab6051ab077c2
spiritsword <18164236+Spiritsword@users.noreply.github.com>
2025-10-17 11:58:58 +0100

    HTML page structure; start of javaScript

---
+++ docs/assets/main.js
@@ -0,0 +1,169 @@
+// GLOBAL DECLARATIONS AND DEFINITIONS
+
+const addButton = document.getElementById("addbutton");
+const taskListCol = document.getElementById("tasklistcol");
+var newTaskInput = document.getElementById("newtask");
+var taskListArray = [];
+
+// CORE FUNCTIONALITY
+
+addButton.addEventListener("click", function() {
+    newTask = newTaskInput.value;
+    if (newTask = ""){
+        alert("Please write new task in the box.")
+    }
+    duplicate = false;
+    tasklistarray.forEach(function (task) {duplicate = (duplicate || newTask==task)})
+    if (duplicate){
+        alert("Please write unique task");
+    }
+    else {
+        addTask(newTask);
+    }
+}
+
+function addTask(newTask) {
+    taskListArray.push(newTask);
+    showTasks(taskListArray);
+}
+
+function taskCompleted(taskID){
+
+}
+
+function showTasks(taskListArray){
+
+}
+
+a
+
+newsDiv.innerHTML = `<h3>${"entertainment".toUpperCase()} NEWS</h3>`;
+  showNews(query, 1);
+});
+
+
+
+
+async function showNews(userChoose, page) {
+    let articles = await getNewsData(apiKey, userChoose, page);
+    articles.forEach(article => createNewArticleDiv(article));
+};
+
+async function getNewsData(apiKey, userChoose, page) {
+    try {
+        let response = await fetch(`https://newsapi.org/v2/everything?q=${userChoose}&pageSize=10&page=${page}&sortBy=popularity&apiKey=${apiKey}`);
+        let data = await response.json();
+        return data.articles;
+    } catch (error) {
+        console.log("No result found ", error);
+    }
+}
+
+function createNewArticleDiv(article) {
+    var articleDiv = document.createElement("div");
+    articleDiv.innerHTML =
+        `<section class="newsarticle card mb-3">
+          <div class="row g-0">
+            <div class="col-md-6">
+              <img src="${article.urlToImage}" class="img-fluid rounded-start" alt="news image">
+            </div>
+            <div class="col-md-6">
+                    <div class="card-body p-0">
+                        <h4 class="card-title">${article.title}</h4>
+                        <p class = "card-text">${article.description}</p>
+                        <p>Published :${new Date(article.publishedAt).toLocaleString()}</p>
+                        <a href="${article.url}" target="_blank" >Read More </a>
+            </div>
+        </section>`;
+    newsDiv.appendChild(articleDiv);
+};
+
+// USER SEARCH
+
+async function filterByUser() {
+    if (userChoose == "") {
+        alert("Please enter what you would like to search");
+        return
+    }
+    currentPage = 1;
+    userChoose = searchInput.value.trim();
+    newsDiv.innerHTML = `<h3>${userChoose.toUpperCase()} NEWS</h3>`;
+    await showNews(userChoose, currentPage);
+}
+
+// CATEGORY SEARCH
+
+var button = document.getElementById("businessID");
+button.addEventListener("click", async function() {
+  var query = "business, company, market, economy, finance";
+  newsDiv.innerHTML = `<h3>${"business".toUpperCase()} NEWS</h3>`;
+
+  showNews(query, 1);
+});
+
+var button = document.getElementById("entertainmentID");
+button.addEventListener("click", async function() {
+  var query = "entertainment, art, culture, movie";
+  newsDiv.innerHTML = `<h3>${"entertainment".toUpperCase()} NEWS</h3>`;
+  showNews(query, 1);
+});
+
+var button = document.getElementById("musicID");    
+button.addEventListener("click", async function() {
+  var query = "music , songs, albums, artists, bands, concerts";
+  newsDiv.innerHTML = `<h3>${"music".toUpperCase()} NEWS</h3>`;
+  showNews(query, 1);
+});
+
+var button = document.getElementById("healthID");   
+button.addEventListener("click", async function() {
+  var query = "health, medicine, fitness, wellness, covid";
+  newsDiv.innerHTML = `<h3>${"health".toUpperCase()} NEWS</h3>`;
+  showNews(query, 1);
+});
+
+var button = document.getElementById("scienceID");
+button.addEventListener("click", async function() {
+  var query = "science";
+  newsDiv.innerHTML = `<h3>${"science".toUpperCase()} NEWS</h3>`;
+  showNews(query, 1);
+});
+
+var button = document.getElementById("sportID");    
+button.addEventListener("click", async function() { 
+  var query = "sport";
+  // var query = "sports, football, cricket, tennis, soccer, basketball, baseball, olympics";
+  newsDiv.innerHTML = `<h3>${"sport".toUpperCase()} NEWS</h3>`;
+  showNews(query, 1);
+});
+
+var button = document.getElementById("technologyID");  
+button.addEventListener("click", async function() {
+  var query = "technology, gadgets, apps, software, hardware, AI, robotics";   
+  newsDiv.innerHTML = `<h3>${"technology".toUpperCase()} NEWS</h3>`;
+  showNews(query, 1);
+});
+
+// PAGE SELECTION
+
+function pagination(pageNumber) {
+    newsDiv.innerHTML = "";
+    if (pageNumber == "prev" && currentPage > 1) {
+        currentPage--;
+    } else if (pageNumber == "next") {
+        currentPage++;
+    } else {
+        currentPage = pageNumber;
+    }
+    showNews(userChoose, currentPage);
+}
+
+// PAGE INITIALISATION
+
+function defaultNews() {
+    searchInput.value = "";
+    showNews(userChoose, currentPage);
+    searchButton.addEventListener("click", filterByUser);
+}
+
+defaultNews()
